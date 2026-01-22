742279eb2d76c063e03daf960c3fdaa932635cca
Demo User <demo@example.com>
2026-01-22 17:38:13 +0100

    Checkpoint from VS Code for cloud agent session

--- CopilotDemo.Api.v2/obj/Debug/net8.0/CopilotDemo.Api.v2.AssemblyInfo.cs
+++ CopilotDemo.Api.v2/obj/Debug/net8.0/CopilotDemo.Api.v2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CopilotDemo.Api.v2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28dc70ced3b23e50715c56bc4b7353cdfb67670e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CopilotDemo.Api.v2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CopilotDemo.Api.v2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
